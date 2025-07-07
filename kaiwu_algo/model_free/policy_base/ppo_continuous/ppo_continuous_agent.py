5f122a10422213cfbbaa341c6b77128e61cdec4a
USTC-RHT <rht@mail.ustc.edu.cn>
2025-07-08 00:32:34 +0800

    ppo_continuous

--- kaiwu_algo/model_free/policy_base/ppo_continuous/ppo_continuous_agent.py
+++ kaiwu_algo/model_free/policy_base/ppo_continuous/ppo_continuous_agent.py
@@ -5,7 +5,7 @@ import torch.nn.functional as F
 from ppo_continuous_algo import Algo
 from ppo_continuous_config import Config
 from dataclasses import dataclass
-from torch.distributions import Normal
+from torch.distributions import Normal, Beta
 
 '''s, a, r, s_next, logprob_a, dw, done'''
 @dataclass
@@ -19,9 +19,9 @@ class SampleData:
     done: bool
 
 # 策略网络构造
-class Actor(torch.nn.Module):
+class GaussianActor(torch.nn.Module):
     def __init__(self, state_dim, hid_shape, action_dim):
-        super(Actor, self).__init__()
+        super(GaussianActor, self).__init__()
         # 1. 先构造共享层（隐藏层）
         layers = []
         layer_shape = [state_dim] + list(hid_shape)
@@ -46,6 +46,51 @@ class Actor(torch.nn.Module):
     def dist(self,x):
         mu, sigma = self.forward(x)
         return Normal(mu,sigma)
+    
+    def deterministic_act(self, state):
+        mu, _ = self.forward(state)
+        return mu
+    
+    def transform_sample_data(self, list_sample_data, device):
+        State = []
+        for sample_data in list_sample_data:
+            State.append(sample_data.state)
+            
+        tensor_state = torch.tensor(np.array(State)).to(device)
+        return tensor_state
+
+class BetaActor(torch.nn.Module):
+    def __init__(self, state_dim, hid_shape, action_dim):
+        super(BetaActor, self).__init__()
+        # 1. 先构造共享层（隐藏层）
+        layers = []
+        layer_shape = [state_dim] + list(hid_shape)
+        activation = nn.Tanh
+        
+        for j in range(len(layer_shape)-1):
+            layers += [nn.Linear(layer_shape[j], layer_shape[j+1]), activation()]
+        
+        # 用Sequential包装共享层
+        self.shared_net = nn.Sequential(*layers)
+        
+        # 2. 分头：mu和sigma分别是两个线性层
+        self.alpha_head = nn.Linear(layer_shape[-1], action_dim)
+        self.beta_head = nn.Linear(layer_shape[-1], action_dim)
+
+    def forward(self, x):
+        x = self.shared_net(x)
+        alpha = F.softplus(self.alpha_head(x)) + 1.0
+        beta = F.softplus(self.beta_head(x)) + 1.0          
+        return alpha, beta
+    
+    def dist(self,x):
+        alpha, beta = self.forward(x)
+        return Beta(alpha, beta)
+    
+    def deterministic_act(self, x):
+        alpha, beta = self.forward(x)
+        mean = (alpha) / (alpha + beta)
+        return mean
     
     def transform_sample_data(self, list_sample_data, device):
         State = []
@@ -94,7 +139,7 @@ class Agent:
         self.critic_learning_rate = Config.critic_learning_rate
         self.device = torch.device("cuda") if torch.cuda.is_available() else torch.device("cpu")
         '''Build Actor and Critic'''
-        self.actor = Actor(self.state_dim,self.actor_hidden_layers,self.action_dim).to(self.device)
+        self.actor = GaussianActor(self.state_dim,self.actor_hidden_layers,self.action_dim).to(self.device)
         self.actor_optimizer = torch.optim.Adam(params=self.actor.parameters(), lr = self.actor_learning_rate)
         self.critic = Critic(self.state_dim,self.critic_hidden_layers).to(self.device)
         self.critic_optimizer = torch.optim.Adam(params=self.critic.parameters(), lr = self.critic_learning_rate)
@@ -120,7 +165,7 @@ class Agent:
         # only used when evaluate the policy.Making the performance more stable
         state = torch.tensor(state, dtype=torch.float32).view(1, self.state_dim).to(self.device)
         with torch.no_grad():
-            mu, _ = self.actor(state)
-            return mu.cpu().numpy()[0]
+            mean = self.actor.deterministic_act(state)
+            return mean.cpu().numpy()[0]
 
     
