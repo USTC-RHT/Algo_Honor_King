2ee1af565058a3a929eccc90c7b8766032df985d
ethanrao <ethanrao@tencent.com>
2025-07-10 15:08:27 +0800

    a2c-0710

--- kaiwu_algo/model_free/policy_base/a2c/a2c_config.py
+++ kaiwu_algo/model_free/policy_base/a2c/a2c_config.py
@@ -5,7 +5,7 @@ class Config:
     critic_hidden_layers = (128)           # critic各个隐藏层的网络规模
     actor_learning_rate = 1e-3             # actor模型学习率
     critic_learning_rate = 1e-2            # critic模型学习率
-    num_episodes = 2000                    # 智能体在环境中运行的序列的数量
+    num_episodes = 1000                    # 智能体在环境中运行的序列的数量
     entropy_coef = 0                       # actor中 entropy loss的系数(熵系数)
     entropy_coef_decay = 0.99              # 熵系数的衰减率
     clip_grad_max_norm = 40                # 梯度裁剪的最大梯度上限
